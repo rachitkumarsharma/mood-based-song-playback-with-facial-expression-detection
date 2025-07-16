da8ee352cbf39dd47ea4746fc02fa402ce3bd71b
Ankur Prajapati <91510065+Ankur77720@users.noreply.github.com>
2025-07-16 22:42:24 +0530

    initialize backend with Express and MongoDB connection add facial expression component styling

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import FacialExpression from "./components/FacialExpression"
 import './App.css'
+import MoodSongs from './components/MoodSongs'
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
   return (
     <>
       <FacialExpression />
+      <MoodSongs />
     </>
   )
 }
