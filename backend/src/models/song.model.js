950dfebfeb174250b157af94b89890b5ec1bffd0
Ankur Prajapati <91510065+Ankur77720@users.noreply.github.com>
2025-07-18 20:43:28 +0530

    add multer and imagekit integration for song upload functionality

---
+++ backend/src/models/song.model.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+
+const songSchema = new mongoose.Schema({
+    title:String,
+    artist:String,
+    audio:String,
+})
+
+
+const song = mongoose.model('song', songSchema)
+
+
+module.exports = song
